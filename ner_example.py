3ca2cd4cd7cfb4ca4da6a38be0a8eeb8935cfc14
Arjun Raj Rajanna <rajannaa@lilac.hpc.private>
2020-08-25 12:41:38 -0400

    update

--- ner_example.py
+++ ner_example.py
@@ -1,13 +1,14 @@
 import nltk
 import csv
 import subprocess
+import regex as re
 
-text = "Smoker with stage IV EGFR mutant lung adenocarcinoma with metastases to lungs, LN, mediastinum and liver, initially responded to afatinib with recent progression of disease (T790M negative) status post craniotomy and brain RT at Mt Sinai Medical Center in 12/2016. He presents to clinic to start carboplatin pemetrexed bevacizumab."
+text = "11/28/16 concurrent chemo with weekly carbo/taxol and RT, completed 5 cycles of weekly chemotherapy    seen in triage for left neck mass, found to have DVT in the left internal jugular vein and cervical lymphadenopathy."
 
 # tokenize
 tokens = nltk.word_tokenize(text)
 
-print(tokens)
+print(tokens, len(tokens))
 
 file_path = open('./datasets/BC5CDR-IOB/test.tsv', 'w')
 writer = csv.writer(file_path, delimiter='\t')
@@ -20,13 +21,15 @@ file_path.close()
 cmd = subprocess.run(['bash', './ner_infer.sh'])
 
 # word predictions
-results_pred_file = './ner_outputs_BC5CDR-IOB/NER_result_conll.txt'
+results_pred_file = './ner_outputs_test/NER_result_conll.txt'
 pred_file = open(results_pred_file, 'r')
 
-final_preds_file = open('./ner_outputs_BC5CDR-IOB/NER_result_preds.tsv', 'w')
+final_preds_file = open('./ner_outputs_test/NER_result_preds.tsv', 'w')
 final_preds_writer = csv.writer(final_preds_file, delimiter='\t')
 for line in pred_file.readlines():
 	items = line.split(' ')
-	final_preds_writer.writerow((items[0], items[2].strip()))
+	print(items, len(items))
+	if len(items) != 1:
+		final_preds_writer.writerow((items[0], items[2].strip()))
 final_preds_file.close()
 
