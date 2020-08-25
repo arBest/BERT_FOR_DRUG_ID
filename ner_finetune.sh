c585c58f6965029bcd356584eb02dd685fccb5de
Arjun Raj Rajanna <rajannaa@lilac.hpc.private>
2020-08-25 12:38:09 -0400

    update

--- ner_finetune.sh
+++ ner_finetune.sh
@@ -4,11 +4,14 @@
 
 # modify - path to pretrained BioBERT model
 export BIOBERT_DIR=./pretrained_weights/biobert_v1.1_pubmed
-echo $BIOBERT_DIR
+
+mv $BIOBERT_DIR/model.ckpt-1000000.data-00000-of-00001 $BIOBERT_DIR/model.ckpt-1000000
+
 # modify - path to dataset
 export NER_DIR=./datasets/BC5CDR-IOB
+
 # modify - create and output results to a output dir
-export OUTPUT_DIR=./ner_outputs_BC5CDR-IOB
+export OUTPUT_DIR=./ner_outputs_test
 mkdir -p $OUTPUT_DIR
 
 python run_ner.py --do_train=true --do_eval=true --vocab_file=$BIOBERT_DIR/vocab.txt --bert_config_file=$BIOBERT_DIR/bert_config.json --init_checkpoint=$BIOBERT_DIR/model.ckpt-1000000 --num_train_epochs=10.0 --data_dir=$NER_DIR --output_dir=$OUTPUT_DIR
--- ner_infer.sh
+++ ner_infer.sh
@@ -8,9 +8,9 @@ echo $BIOBERT_DIR
 # modify - dataset
 export NER_DIR=./datasets/BC5CDR-IOB
 # modify - output
-export OUTPUT_DIR=./ner_outputs_BC5CDR-IOB
+export OUTPUT_DIR=./ner_outputs_test
 
-python run_ner.py --do_train=false --do_eval=false --do_predict=true --vocab_file=$BIOBERT_DIR/vocab.txt --bert_config_file=$BIOBERT_DIR/bert_config.json --init_checkpoint=/home/rajannaa/biobert/ner_outputs_BC5CDR-chem-IOB/model.ckpt-1425 --data_dir=$NER_DIR --output_dir=$OUTPUT_DIR
+python run_ner.py --do_train=false --do_eval=false --do_predict=true --vocab_file=$BIOBERT_DIR/vocab.txt --bert_config_file=$BIOBERT_DIR/bert_config.json --init_checkpoint=$OUTPUT_DIR/model.ckpt-1425 --data_dir=$NER_DIR --output_dir=$OUTPUT_DIR
 
 python biocodes/ner_detokenize.py --token_test_path=$OUTPUT_DIR/token_test.txt --label_test_path=$OUTPUT_DIR/label_test.txt --answer_path=$NER_DIR/test.tsv --output_dir=$OUTPUT_DIR
 
